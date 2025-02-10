7e1eff79dd2528e97b1e93954d60f497f2ed9117
haegi <b_haegi@naver.com>
2025-02-10 16:07:38 +0900

    날씨 데이터 받아와보기

--- src/pages/weather/Weather.jsx
+++ src/pages/weather/Weather.jsx
@@ -1,7 +1,8 @@
 import Search from "./Search";
 import WeatherInfo from "./WeatherInfo";
-import { AiFillCaretDown, AiOutlineMenu } from "react-icons/ai";
-import { useState } from "react";
+import { AiFillCaretDown } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Weather() {
     const [favorites, setFavorites] = useState(false);
@@ -9,23 +10,30 @@ export default function Weather() {
      // 즐겨찾기 도시 리스트 (임시 더미 데이터)
      const favoriteCities = ["서울", "대전", "대구", "부산", "광주"];
 
+     useEffect(() => {
+        const fetchWeather = async () => {
+          try {
+            const response = await axios.get("http://localhost:8080/api/weather", {
+              params: { city: "부산" }
+            });
+            console.log(response.data);
+          } catch (err) {
+            console.error("Error fetching weather data:", err);
+          }
+        };
+        fetchWeather();
+      }, []);
+
     return (
         <>
-            <header>
-                <nav className="flex font-bold items-center justify-between h-16">
-                    <h1 className="text-lg">오늘의 날씨</h1>
-                    <span><AiOutlineMenu /></span>
-                </nav>
-            </header>
-
             <main>
                 <Search />
                 <WeatherInfo />
                 
-                <div className="border-t-2 border-gray-300 mt-5">
+                <div className=" mt-5">
                     <div
                         onClick={() => setFavorites(!favorites)}
-                        className="flex items-center justify-center cursor-pointer text-center mt-5 p-1 bg-gray-300 rounded-xl">
+                        className="flex items-center justify-center cursor-pointer text-center mt-5 p-1 bg-blue-400 text-white rounded-xl">
                         <h1 className="mr-1">즐겨찾기 한 도시</h1>
                         <span className={`transform transition-transform duration-300 ${favorites ? "rotate-180" : "rotate-0"}`}>
                             <AiFillCaretDown />
